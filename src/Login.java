0a629cf3c78c63956faeb7177dd3053a2a76e7e6
Francisco Garache <franciscogarach3@gmail.com>
2022-04-27 15:46:19 -0600

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
